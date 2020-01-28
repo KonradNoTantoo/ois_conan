939b7651537f313d610f25626b83741e9cd6b0dd
Knife <konrad@no.tantoo>
2020-01-28 22:50:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Conan recipe for ois 1.5.0
+
+[![Build Status: Windows](https://ci.appveyor.com/api/projects/status/github/KonradNoTantoo/ois_conan?svg=true)](https://ci.appveyor.com/project/KonradNoTantoo/ois-conan)
+
+[![Build Status: Linux](https://api.travis-ci.org/KonradNoTantoo/ois_conan.svg?branch=master)](https://travis-ci.org/KonradNoTantoo/ois_conan)
+
+### Description
+A test recipe for now...
+
+### Repository
+Published on [Utopia bintray](https://bintray.com/konradnotantoo/utopia/):
+```
+conan remote add utopia https://api.bintray.com/conan/konradnotantoo/utopia
+```
+
+### Recipe license
+MIT
---
+++ conanfile.py
@@ -0,0 +1,43 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class OisConan(ConanFile):
+    name = "ois"
+    version = "1.5"
+    license = "zlib"
+    author = "konrad.no.tantoo"
+    url = ""
+    description = "Cross Platform Object Oriented Input Lib System. Meant to be very robust and compatible with many systems and operating systems."
+    topics = ("User input", "UI")
+    settings = "os", "compiler", "build_type", "arch"
+    # options = {"shared": [True, False]}
+    # default_options = "shared=False"
+    generators = "cmake"
+    _source_subfolder = "source_subfolder"
+
+    def source(self):
+        tools.get("https://github.com/wgois/OIS/archive/v{}.tar.gz".format(self.version))
+        os.rename("OIS-{}".format(self.version), self._source_subfolder)
+
+        # This small hack might be useful to guarantee proper /MT /MD linkage
+        # in MSVC if the packaged project doesn't have variables to set it
+        # properly
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+            "project(OIS)",
+            '''PROJECT(OIS)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()''')
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=self._source_subfolder)
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["ois_d" if self.settings.build_type == "Debug" else "ois"]
+        self.cpp_info.includedirs = [os.path.join("include", "ois")]
