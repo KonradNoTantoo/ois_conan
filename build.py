f822bdf43faf1b47d0cf92dc9ddc0f3c9c716c05
Knife <konrad@no.tantoo>
2020-01-29 22:44:30 +0100

    Disable x86 builds

--- build.py
+++ build.py
@@ -4,4 +4,5 @@ from cpt.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds()
+    builder.remove_build_if(lambda build: build.settings["arch"] == "x86")
     builder.run()
